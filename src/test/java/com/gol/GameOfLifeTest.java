7bdc102cf0e200a208fa80a4692e6dfb6429e35b
Marco Selig <marco.selig@de.ibm.com>
2017-10-04 18:05:22 +0200

    final test edited

--- src/test/java/com/gol/GameOfLifeTest.java
+++ src/test/java/com/gol/GameOfLifeTest.java
@@ -1,25 +1,17 @@
 package com.gol;
 
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
-
 import org.junit.Test;
 
 
 public class GameOfLifeTest
 {
     @Test
-    public void testConstructorExceptions()
+    public void testGameOfLife()
     {
         try
         {
-            GameOfLife.main(new String[]{"nan"});
-            fail("expect: NumberFormatException");
-        }
-        catch(NumberFormatException exception)
-        {
-            assertEquals(exception.getMessage(), "For input string: \"nan\"");
+            GameOfLife.main(null);
         }
         catch(Exception exception)
         {
